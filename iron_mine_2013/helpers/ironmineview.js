72d030ad8f9c10d2afaa4848c83e3d1a982fc101
Phildo <phildo211@gmail.com>
2013-09-16 10:43:06 -0500

    animated gurus with correct font

--- iron_mine_2013/helpers/ironmineview.js
+++ iron_mine_2013/helpers/ironmineview.js
@@ -80,7 +80,7 @@ var IronMineView = function()
         this.currentVid      = this.vids[game];
         this.currentIntro    = this.intros[game];
         this.currentActivity = this.activities[game];
-        this.currentGuru     = this.gurus[game];
+        this.currentGuru     = this.gurus[game]; this.currentGuru.progress = 50;
         this.currentGuruTalk = this.gurutalks[game];
 
         this.currentGame.style.display = 'block';
@@ -113,23 +113,46 @@ var IronMineView = function()
 
     this.displayGuruWithMessage = function(message)
     {
-        if(this.currentGuru)
-        {
-            this.currentGuru.style.display = 'block';
-            this.currentGuruTalk.innerHTML = message;
-        }
+        self.currentGuru.style.display = 'block';
+        self.currentGuruTalk.innerHTML = message;
+
+        var alreadyTicking = (self.currentGuru.progress != 50);
+        self.currentGuru.progress = 0;
+        if(!alreadyTicking) tickdisplayguru();
+    }
+
+    function tickdisplayguru()
+    {
+        self.currentGuru.progress++;
+        self.currentGuru.style.top = (10*(50-self.currentGuru.progress))+'px';
+        if(self.currentGuru.progress < 50)
+            setTimeout(tickdisplayguru, 10);
     }
 
     this.hideGuru = function()
     {
-        if(this.currentGuru) this.currentGuru.style.display = 'none';
+        if(self.currentGuru)
+        {
+            var alreadyTicking = (self.currentGuru.progress != 50);
+            self.currentGuru.progress = 0;
+            if(!alreadyTicking) tickhideguru();
+        }
+    }
+    
+    function tickhideguru()
+    {
+        self.currentGuru.progress++;
+        self.currentGuru.style.top = (10*self.currentGuru.progress)+'px';
+        if(self.currentGuru.progress < 50)
+            setTimeout(tickhideguru,10);
+        else
+            self.currentGuru.style.display = 'none';
     }
 
     this.displayMoneyDelta = function(delta)
     {
         if(delta == 0) return;
 
-        var alreadyTicking = false;
         
         if(delta > 0) self.moneydelta.style.color = "#009344";
         else          self.moneydelta.style.color = "#C42032";
@@ -137,7 +160,7 @@ var IronMineView = function()
 
         if(delta < 0) delta *= -1;//so we can parse it to text without worrying about - signs
         self.moneydelta.innerHTML = (self.moneydelta.delta > 0 ? '+$' : '-$')+((delta-(delta%100))/100)+'.'+(delta%100 < 10 ? '0' : '')+(delta%100);
-        if(self.moneydelta.fade != 0) alreadyTicking = true;
+        var alreadyTicking = (self.moneydelta.fade != 0);
         self.moneydelta.fade = 120;
         self.moneydelta.style.display = 'block';
         if(!alreadyTicking) tickmoneydelta();
@@ -160,14 +183,10 @@ var IronMineView = function()
         if(self.moneydelta.fade > 0)
             setTimeout(tickmoneydelta, 10);
         else
-            hidemoneydelta();
-    }
-
-    var hidemoneydelta = function()
-    {
-        self.moneydelta.fade = 0;
-        self.moneydelta.style.color = 'rgba(255,255,255,0.0)';
-        self.moneydelta.style.display = 'none';
+        {
+            self.moneydelta.style.color = 'rgba(255,255,255,0.0)';
+            self.moneydelta.style.display = 'none';
+        }
     }
 
 }

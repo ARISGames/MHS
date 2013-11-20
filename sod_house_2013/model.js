ff042556957b2a291b165d733267fec34ad8a82e
Phildo <phildo211@gmail.com>
2013-11-20 12:04:13 -0600

    added sod house webpage

---
+++ sod_house_2013/model.js
@@ -0,0 +1,49 @@
+var SodHouseModel = function()
+{
+    var self = this;
+
+    self.gameId = 0; 
+    self.player = {};
+    self.webPageId = 0;
+
+    self.currentLevel = 1;
+
+    self.loadStateFromARIS = function(callback)
+    {
+        var bogusEndOfQueueId = 99999999; //Used to flag the end of the queue
+
+        //Override to handle ARIS responses
+        ARIS.didUpdateItemQty = function(updatedItemId, qty)
+        {
+            if(updatedItemId == bogusEndOfQueueId)
+                callback();
+        };
+
+        ARIS.didReceivePlayer = function(player)
+        {
+            self.player = player;
+        }
+
+        var params = ARIS.parseURLParams(document.URL);
+        self.gameId = parseInt(params.gameId);
+        self.player.playerId = parseInt(params.playerId);
+        self.webPageId = parseInt(params.webPageId);
+
+        ARIS.getPlayer();
+        ARIS.getItemCount(bogusEndOfQueueId); //Enqueued to signal the queue to 'get state' has sufficiently advanced
+    }
+
+    self.sendRequest = function(fn, callback)
+    {
+        var xmlhttp;
+        xmlhttp=new XMLHttpRequest();
+        xmlhttp.open("GET","http://arisgames.org/server/json.php/v1."+fn,true); 
+        xmlhttp.onreadystatechange = function()
+        {
+            if(xmlhttp.readyState == 4&& xmlhttp.status == 200)
+                callback(JSON.parse(xmlhttp.responseText).data);
+        }
+        xmlhttp.send();
+    }
+}
+
---
+++ sod_house_2013/views.js
@@ -0,0 +1,19 @@
+var FurTradeViews = function()
+{
+    var self = this;
+
+    self.displayLoading = function()
+    {
+        document.getElementById('vidfile').pause();
+        document.getElementById('vid').style.display     = 'none';
+        document.getElementById('loading').style.display = 'block';
+    }
+
+    self.displayVid = function()
+    {
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('vid').style.display     = 'block';
+        document.getElementById('vidfile').play();
+    }
+}
+

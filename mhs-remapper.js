df67e8c257c2b4b767c2731c99c61274b5809af9
Kevin Alford <zeroeth@pixelflow.org>
2015-06-01 16:38:07 -0500

    Node app to remap

---
+++ mhs-remapper.js
@@ -0,0 +1,67 @@
+/* Node app to remap IDs from v1 game to v2
+ *
+ * - Run migrate. Get the response JSON and save it to a file.
+ * - Run getQuestsForGame and save it to a file.
+ * - Run this to translate to v2 ids in javascript/json files.
+ * - Point all v2 game web page urls to new instance of javascript.
+ * - Update quests to new versions.
+ */
+
+/* Important files
+ *
+ * ack -l "\w+Id\(\d+?\)"
+ * ack -l "exitTo\w+\(\d+\)"
+ */
+
+var fs   = require('fs');
+var path = require('path');
+
+var json_mapping_file = "mhs-ptp-migration-map-game.json";
+
+var files_to_remap = [
+	"mhs-ptp-quest.json",
+	"fur_trade/model.js",
+	"iron_mine/helpers/ironminemodel.js",
+	"iron_mine/index.php",
+	"sod_house/index.html"
+];
+
+
+var Remapper = function()
+{
+	this.run = function()
+	{
+		var mapper = this;
+		var filename = path.join(__dirname, json_mapping_file);
+
+		fs.readFile(filename, 'utf8', function(error, data)
+		{
+			mapper.remap_files(JSON.parse(data));
+		});
+	};
+
+
+	this.remap_files = function(game_map_json)
+	{
+		var mapper = this;
+
+		files_to_remap.forEach(function(remapping_file)
+		{
+			var filename = path.join(__dirname, remapping_file);
+			fs.readFile(filename, 'utf8', function(error, data)
+			{
+				var remapped_string = mapper.remap_string(data);
+			});
+		});
+	};
+
+
+	this.remap_string = function(string)
+	{
+		var remapped_string = string;
+
+		return remapped_string;
+	};
+};
+
+new Remapper().run();

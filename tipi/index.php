8a3de24f36f9caf3441645a5830f0675fc338e03
Michael Tolly <miketolly@gmail.com>
2016-06-02 11:41:25 -0500

    Intro video for Tipi

---
+++ tipi/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="no-cache" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width; initial-scale=1.0 maximum-scale=1.0 user-scalable=no;"/>
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<title>Tipi</title>
+<link href="style.css" rel="stylesheet" type="text/css"></link>
+
+<script type="text/javascript">
+    var ARIS = {};//Get ARIS variable existant ASAP just to keep things simple
+</script>
+
+<script type="text/javascript" src="model.js"></script>
+<script type="text/javascript" src="views.js"></script>
+<script type="text/javascript">
+var game_id = webHookId = npcId = function(id){return id;};
+
+var shm;
+var shv;
+
+var readyCount = 2;
+function partReady() { readyCount--; if(readyCount == 0) begin(); }
+ARIS.ready = function() { partReady(); };
+
+
+function begin()
+{
+    shm = new TipiModel();
+    shv = new TipiViews();
+    shm.loadStateFromARIS(stateReceived);
+}
+
+function stateReceived()
+{
+    ARIS.didUpdateItemQty = function(updatedItemId,qty) //simplify item updates after initial state received
+    {
+        if(o = shm.itemForItemId(updatedItemId)) o.qty = qty;
+    }
+
+    shv.displayVid();
+}
+
+
+var timevidstarted = new Date();
+function vidComplete()
+{  
+    //log that vid was skipped
+    var timevidskipped = new Date();
+    var secondselapsed = ((timevidskipped-timevidstarted)/1000);
+    if(secondselapsed < 20)
+    {
+        //var xmlhttp;
+        //xmlhttp=new XMLHttpRequest();
+        //xmlhttp.open("GET","http://arisgames.org/server/json.php/v1.webhooks.setWebHookReq/"+game_id(5252)+"/"+webHookId(992)+"/0/"+shm.player.user_id,true); 
+        //xmlhttp.send();
+    }
+
+    ARIS.exitToCharacter(npcId(27870));
+}
+
+document.addEventListener( "DOMContentLoaded", partReady, false );
+window.onerror = function(msg, url, linenumber) 
+{
+    //alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+};
+</script>
+</head>
+
+<body class='full_screen'>
+
+<div id='loading' class='full_screen'>&nbsp;<img height='12px' src='assets/spinner.gif'></img> Loading...</div>
+
+<div id='vid' class='full_screen'>
+    <video id="vidfile" width="320" height="504" webkit-playsinline>
+        <source src="assets/intro.mp4" type="video/mp4">
+    </video>
+    <div class='bottombutton' ontouchstart='vidComplete();'>
+        <img src='assets/forward_arrow.png' class='forwardarrow' />
+    </div>
+</div>
+
+</body>
+</html>
+
---
+++ tipi/model.js
@@ -0,0 +1,50 @@
+var TipiModel = function()
+{
+    var self = this;
+
+    self.game_id = 0; 
+    self.player = {};
+    self.web_page_id = 0;
+
+    self.currentLevel = 1;
+
+    self.loadStateFromARIS = function(callback)
+    {
+        var bogusEndOfQueueId = 99999999; //Used to flag the end of the queue
+
+        //Override to handle ARIS responses
+        ARIS.didUpdateItemQty = function(updatedItemId, qty)
+        {
+            if(updatedItemId == bogusEndOfQueueId)
+                callback();
+        };
+
+        ARIS.didReceivePlayer = function(player)
+        {
+            self.player = player;
+        }
+
+        var params = ARIS.parseURLParams(document.URL);
+        self.game_id = parseInt(params.game_id);
+        self.player.user_id = parseInt(params.user_id);
+        self.web_page_id = parseInt(params.web_page_id);
+
+        ARIS.getPlayer();
+        ARIS.getItemCount(bogusEndOfQueueId); //Enqueued to signal the queue to 'get state' has sufficiently advanced
+    }
+
+	// FIXME dead?
+    self.sendRequest = function(fn, callback)
+    {
+        var xmlhttp;
+        xmlhttp=new XMLHttpRequest();
+        xmlhttp.open("GET","http://arisgames.org/server/json.php/v1."+fn,true); 
+        xmlhttp.onreadystatechange = function()
+        {
+            if(xmlhttp.readyState == 4&& xmlhttp.status == 200)
+                callback(JSON.parse(xmlhttp.responseText).data);
+        }
+        xmlhttp.send();
+    }
+}
+

4a62c848c3fb294ef87dcfa64cd1cef3f14a743e
Michael Tolly <miketolly@gmail.com>
2017-03-07 16:24:40 -0600

    Working linguist game

--- ww2/linguist/script/Game.js
+++ ww2/linguist/script/Game.js
@@ -10,6 +10,10 @@ ENGINE.Game = {
 
   create: function() {
     this.question = 1;
+    this.right = 0;
+    this.wrong = 0;
+    this.popup = null;
+    this.buttons = [];
   },
 
   step: function(dt) {
@@ -18,8 +22,13 @@ ENGINE.Game = {
   pointerdown: function(event) {
     var app = this.app;
     var self = this;
-    this.question++;
-    if (this.question > 3) this.question = 1;
+    self.buttons.forEach(function(button){
+      if (!button.maxX) button.maxX = button.minX + button.width;
+      if (!button.maxY) button.maxY = button.minY + button.height;
+      if (button.minX <= event.x && event.x <= button.maxX && button.minY <= event.y && event.y <= button.maxY) {
+        button.fn.bind(self)();
+      }
+    });
   },
 
   textBook1: function(bookMinX, bookWidth, bookHeight) {
@@ -41,6 +50,33 @@ ENGINE.Game = {
     layer.fillText('East', bookMinX + bookWidth * 0.29, bookHeight * 0.62);
     layer.fillText('South', bookMinX + bookWidth * 0.69, bookHeight * 0.32);
     layer.fillText('West', bookMinX + bookWidth * 0.69, bookHeight * 0.62);
+
+    return [
+      { minX: bookMinX + bookWidth * 0.12,
+        minY: bookHeight * 0.16,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.2,
+        fn: function(){ this.popup = 'wrong1'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.52,
+        minY: bookHeight * 0.16,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.2,
+        fn: function(){ this.popup = 'wrong1'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.12,
+        minY: bookHeight * 0.46,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.2,
+        fn: function(){ this.popup = 'right1'; this.right++; }
+      },
+      { minX: bookMinX + bookWidth * 0.52,
+        minY: bookHeight * 0.46,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.2,
+        fn: function(){ this.popup = 'wrong1'; this.wrong++; }
+      },
+    ];
   },
 
   textBook2: function(bookMinX, bookWidth, bookHeight) {
@@ -60,6 +96,45 @@ ENGINE.Game = {
     layer.fillText('Thursday', bookMinX + bookWidth * 0.55, bookHeight * 0.28);
     layer.fillText('Friday', bookMinX + bookWidth * 0.55, bookHeight * 0.46);
     layer.fillText('Saturday', bookMinX + bookWidth * 0.55, bookHeight * 0.64);
+
+    return [
+      { minX: bookMinX + bookWidth * 0.12,
+        minY: bookHeight * 0.155,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.16,
+        fn: function(){ this.popup = 'wrong2'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.52,
+        minY: bookHeight * 0.155,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.16,
+        fn: function(){ this.popup = 'wrong2'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.12,
+        minY: bookHeight * 0.335,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.16,
+        fn: function(){ this.popup = 'wrong2'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.52,
+        minY: bookHeight * 0.335,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.16,
+        fn: function(){ this.popup = 'wrong2'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.12,
+        minY: bookHeight * 0.515,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.16,
+        fn: function(){ this.popup = 'right2'; this.right++; }
+      },
+      { minX: bookMinX + bookWidth * 0.52,
+        minY: bookHeight * 0.515,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.16,
+        fn: function(){ this.popup = 'right2'; this.right++; }
+      },
+    ]
   },
 
   textBook3: function(bookMinX, bookWidth, bookHeight) {
@@ -77,6 +152,110 @@ ENGINE.Game = {
     layer.fillStyle('black');
     layer.fillText('bombers', bookMinX + bookWidth * 0.3, bookHeight * 0.55);
     layer.fillText('fighter planes', bookMinX + bookWidth * 0.7, bookHeight * 0.55);
+
+    return [
+      { minX: bookMinX + bookWidth * 0.12,
+        minY: bookHeight * 0.28,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.3,
+        fn: function(){ this.popup = 'wrong3'; this.wrong++; }
+      },
+      { minX: bookMinX + bookWidth * 0.52,
+        minY: bookHeight * 0.28,
+        width: bookWidth * 0.36,
+        height: bookHeight * 0.3,
+        fn: function(){ this.popup = 'right3'; this.right++; }
+      },
+    ];
+  },
+
+  drawPopup: function() {
+    var app = this.app;
+    var layer = this.app.layer;
+    layer.fillStyle('rgba(255,255,255,0.6)');
+    layer.fillRect(0, 0, app.width, app.height);
+    var popupHeight = app.height * 0.6;
+    layer.fillStyle('black');
+    layer.fillRect(0, (app.height - popupHeight) / 2, app.width, popupHeight);
+    layer.fillStyle('#ccc');
+    layer.fillRect(0, (app.height - popupHeight) / 2 + popupHeight * 0.02, app.width, popupHeight - popupHeight * 0.04);
+    var correct = this.popup[0] === 'r';
+    layer.fillStyle(correct ? 'green' : 'red');
+    layer.textAlign('center');
+    layer.font(Math.floor(app.height * 0.08) + 'px sans-serif');
+    layer.fillText((correct ? 'Correct!' : 'Incorrect'), app.width / 2, app.height * 0.35);
+    layer.fillStyle('black');
+    layer.font(Math.floor(app.height * 0.038) + 'px sans-serif');
+    var line1, line2, line3, line4;
+    switch (this.popup) {
+      case 'right1':
+        line1 = 'The US Navy sunk the';
+        line2 = 'ship before it attacked.';
+        line3 = '100 lives saved!';
+        line4 = '';
+        break;
+      case 'right2':
+        line1 = 'The Allies captured the';
+        line2 = 'general and stopped his';
+        line3 = 'next attack!';
+        line4 = '200 lives saved!';
+        break;
+      case 'right3':
+        line1 = 'The US shot down';
+        line2 = 'all 150 planes.';
+        line3 = '500 lives saved!';
+        line4 = '';
+        break;
+      case 'wrong1':
+        line1 = 'The Japanese ship';
+        line2 = 'attacked the US ship. 100';
+        line3 = 'US Navy men injured. Look';
+        line4 = 'closely next time!';
+        break;
+      case 'wrong2':
+        line1 = 'The general attacked.';
+        line2 = '4 islands lost to the';
+        line3 = 'Japanese. Be more';
+        line4 = 'careful next time!';
+        break;
+      case 'wrong3':
+        line1 = '';
+        line2 = 'The 150 fighter planes';
+        line3 = 'attacked. 3 ships lost.';
+        line4 = '';
+        break;
+    }
+    layer.fillText(line1, app.width / 2, app.height * 0.46);
+    layer.fillText(line2, app.width / 2, app.height * 0.55);
+    layer.fillText(line3, app.width / 2, app.height * 0.64);
+    layer.fillText(line4, app.width / 2, app.height * 0.73);
+    layer.font(Math.floor(app.height * 0.05) + 'px sans-serif');
+    layer.fillStyle('white');
+    layer.fillText('TAP TO CONTINUE', app.width / 2 - 4, app.height * 0.9 - 4);
+    layer.fillText('TAP TO CONTINUE', app.width / 2 - 4, app.height * 0.9 + 4);
+    layer.fillText('TAP TO CONTINUE', app.width / 2 + 4, app.height * 0.9 - 4);
+    layer.fillText('TAP TO CONTINUE', app.width / 2 + 4, app.height * 0.9 + 4);
+    layer.fillStyle('black');
+    layer.fillText('TAP TO CONTINUE', app.width / 2, app.height * 0.9);
+    return [
+      { minX: 0,
+        minY: 0,
+        maxX: app.width,
+        maxY: app.height,
+        fn: function(){
+          if (this.question === 3) {
+            if (this.wrong <= 1) {
+              exitToDialog(90815);
+            } else {
+              exitToDialog(91061);
+            }
+          } else {
+            this.question++;
+            this.popup = null;
+          }
+        }
+      }
+    ];
   },
 
   render: function() {
@@ -96,10 +275,11 @@ ENGINE.Game = {
     layer.drawImage(app.images.paper, 0, paperMinY, app.width, paperMaxY - paperMinY);
     layer.drawImage(app.images.book, bookMinX, 0, bookWidth, bookHeight);
 
+    var bookButtons;
     switch (this.question) {
-      case 1: this.textBook1(bookMinX, bookWidth, bookHeight); break;
-      case 2: this.textBook2(bookMinX, bookWidth, bookHeight); break;
-      case 3: this.textBook3(bookMinX, bookWidth, bookHeight); break;
+      case 1: bookButtons = this.textBook1(bookMinX, bookWidth, bookHeight); break;
+      case 2: bookButtons = this.textBook2(bookMinX, bookWidth, bookHeight); break;
+      case 3: bookButtons = this.textBook3(bookMinX, bookWidth, bookHeight); break;
     }
 
     var jpnImage = app.images['japanese' + this.question];
@@ -113,6 +293,30 @@ ENGINE.Game = {
     var engWidth = Math.min(app.width, engImage.width * (engHeight / engImage.height));
     engHeight = engImage.height * (engWidth / engImage.width);
     layer.drawImage(engImage, (app.width - engWidth) / 2, paperMaxY, engWidth, engHeight);
+
+    switch (this.popup) {
+      case 'right1':
+        this.buttons = this.drawPopup();
+        break;
+      case 'wrong1':
+        this.buttons = this.drawPopup();
+        break;
+      case 'right2':
+        this.buttons = this.drawPopup();
+        break;
+      case 'wrong2':
+        this.buttons = this.drawPopup();
+        break;
+      case 'right3':
+        this.buttons = this.drawPopup();
+        break;
+      case 'wrong3':
+        this.buttons = this.drawPopup();
+        break;
+      default:
+        this.buttons = bookButtons;
+        break;
+    }
   }
 
 };

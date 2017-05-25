a92bd1850b98ab35682a3ca326b80cafd0c25b9e
Michael Tolly <miketolly@gmail.com>
2017-05-25 17:41:05 -0500

    Start wasp game without waiting for aris

--- ww2/wasp/script/Main.js
+++ ww2/wasp/script/Main.js
@@ -1,35 +1,31 @@
-var ARIS = {};
-ARIS.ready = function(){
-  var app = new PLAYGROUND.Application({
+var app = new PLAYGROUND.Application({
 
-    smoothing: true,
+  smoothing: true,
 
-    create: function() {
+  create: function() {
 
-      /* things to preload */
+    /* things to preload */
 
-      this.loadImage("winglevel");
-      this.loadImage("altimeter");
-      this.loadImage("stick_up");
-      this.loadImage("stick_down");
-      this.loadImage("stick_left");
-      this.loadImage("stick_right");
-      this.loadImage("clouds.jpg");
-      this.loadImage("pilot_bad.jpg");
-      this.loadImage("pilot_good.jpg");
-      this.loadImage("clipboard");
-      this.loadImage("check");
+    this.loadImage("winglevel");
+    this.loadImage("altimeter");
+    this.loadImage("stick_up");
+    this.loadImage("stick_down");
+    this.loadImage("stick_left");
+    this.loadImage("stick_right");
+    this.loadImage("clouds.jpg");
+    this.loadImage("pilot_bad.jpg");
+    this.loadImage("pilot_good.jpg");
+    this.loadImage("clipboard");
+    this.loadImage("check");
 
-    },
+  },
 
-    ready: function() {
+  ready: function() {
 
-      /* after preloading route events to the game state */
+    /* after preloading route events to the game state */
 
-      this.setState(ENGINE.Checklist);
+    this.setState(ENGINE.Checklist);
 
-    }
+  }
 
-  });
-};
-//ARIS.ready();
+});
